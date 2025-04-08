c1540f828febc0e72e4d422783d11b84bc9698c0
HyoSangByun <billy04100410@gmail.com>
2025-04-08 13:11:44 +0900

    refactor: router 파일 위치 변경

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import './assets/main.css'
+import './assets/main.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import 'bootstrap';
-import router from './components/router';
+import router from '../src/router';
 const app = createApp(App);
 
 app.use(router);
