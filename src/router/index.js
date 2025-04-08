d5498e247a5c1cb0927c6adfc6bc5d8781c376ea
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2025-04-08 13:09:55 +0900

    feat: login page 경로 수정

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: '/',
+      path: '/login',
       name: 'Login',
       components: {
         default: Login,
