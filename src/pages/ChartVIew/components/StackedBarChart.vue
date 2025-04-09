fc49a5ae688e8611c5fb9838d792a619027f1a30
HyoSangByun <billy04100410@gmail.com>
2025-04-09 14:51:01 +0900

    feat: 전월 비교 차트

---
+++ src/pages/ChartVIew/components/StackedBarChart.vue
@@ -0,0 +1,33 @@
+<template>
+  <div v-if="chartData && chartData.labels && chartData.datasets">
+    <Bar :data="chartData" :options="chartOptions" />
+  </div>
+</template>
+
+<script setup>
+import { Bar } from 'vue-chartjs';
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
+
+// Props
+const props = defineProps({
+  chartData: Object,
+  chartOptions: Object,
+});
+</script>
+
+<style scoped>
+div {
+  position: relative;
+  width: 100%;
+  min-height: 300px;
+}
+</style>
